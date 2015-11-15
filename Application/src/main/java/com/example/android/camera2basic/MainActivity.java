299eb3bd2ca1172b20fd0eddeb37156e5c5f631e
Brijesh Rakholia <brijeshrakholia@dhcp020-208-235.wireless.buffalo.edu>
2015-11-14 20:59:40 -0500

    mainactivity

--- Application/src/main/java/com/example/android/camera2basic/MainActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.android.camera2basic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intent = new Intent(this, CameraActivity.class);
+        Button cameraButton = (Button) findViewById(R.id.button_id);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 
 }
